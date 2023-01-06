2392bd0eb82e700cbdbb96fcf94711f02f3d8d3e
Cleidson Franklin <cleidsonads.dev@gmail.com>
2023-01-05 22:38:03 -0300

    atualizações 05/01

---
+++ main.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('#main__apr', {duration: 2000});
+ScrollReveal().reveal('.card_projeto', {duration: 1000});
+ScrollReveal().reveal(' .img_main', {duration: 1000});
+ScrollReveal().reveal(' .card1', {duration: 1000});
+ScrollReveal().reveal(' .title', {duration: 500});
+
+
+
+
